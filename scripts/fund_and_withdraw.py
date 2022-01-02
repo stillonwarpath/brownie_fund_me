4f725a8b4e3dfa11e838e2928ccf2914c0486a36
francisco <fr.betancourt@live.com>
2022-01-02 17:56:05 -0300

    Function call removed because it is not necesary

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -6,7 +6,6 @@ def fund():
     fund_me = FundMe[-1]
     account = get_account()
     entrance_fee = fund_me.getEntranceFee()
-    price = fund_me.getPrice()
     print(f"The current entry fee is {entrance_fee}")
     print("Funding")
     fund_me.fund({"from": account, "value": entrance_fee})
