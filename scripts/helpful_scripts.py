a221fd0cb0740c9b275c571a2d8e52d08139e731
francisco <fr.betancourt@live.com>
2022-01-02 15:28:49 -0300

    Updated get account for using account based on local blockchain

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -8,7 +8,7 @@ STARTING_PRICE = 20
 
 
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
