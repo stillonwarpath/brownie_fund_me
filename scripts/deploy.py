62115fe882c1561537d81b8855df4bcca5f4d18e
francisco <fr.betancourt@live.com>
2022-01-02 13:29:47 -0300

    Get the price feed contract address based on the network we are deploying

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,12 +1,19 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
 from scripts.helpful_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
     # pass the price feed address to our fundme contract
+    # if we are on a persistent network like rinkeby, use the associated address
+    # otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else
     fund_me = FundMe.deploy(
-        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        price_feed_address,
         {"from": account},
         publish_source=True,
     )
