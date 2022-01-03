145e4acddd5397c658da324b2243d3baad4851e2
francisco <fr.betancourt@live.com>
2022-01-03 16:08:59 -0300

    Added test for fund and withdraw

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -11,5 +11,12 @@ def fund():
     fund_me.fund({"from": account, "value": entrance_fee})
 
 
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
+
+
 def main():
     fund()
+    withdraw()
