67ce9f40d90fecf928f3ebfebbe9b0f90ee638d5
francisco <fr.betancourt@live.com>
2022-01-03 16:24:14 -0300

    Added test for an account other than owner tries to withdraw.

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,5 +1,7 @@
-from scripts.helpful_scripts import get_account
+from brownie import network, accounts, exceptions
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
 from scripts.deploy import deploy_fund_me
+import pytest
 
 
 def test_can_fund_and_withdraw():
@@ -13,3 +15,12 @@ def test_can_fund_and_withdraw():
     tx2.wait(1)
     assert fund_me.addressToAmountFunded(account.address) == 0
     tx2.wait(1)
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
